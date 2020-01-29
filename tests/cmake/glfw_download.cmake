6c757643ee524cf6ae745e0bb9f576205bd42a02
irov <irov13@mail.ru>
2020-01-29 13:21:20 +0200

    fix

--- tests/cmake/glfw_download.cmake
+++ tests/cmake/glfw_download.cmake
@@ -13,7 +13,7 @@ ExternalProject_Add(glfw_download PREFIX glfw
             -DGLFW_BUILD_EXAMPLES=OFF
             -DGLFW_BUILD_TESTS=OFF
             -DGLFW_BUILD_DOCS=OFF
-            -DGLFW_INSTAL=ON
+            -DGLFW_INSTALL=ON
             -DGLFW_VULKAN_STATIC=OFF
             
     )
